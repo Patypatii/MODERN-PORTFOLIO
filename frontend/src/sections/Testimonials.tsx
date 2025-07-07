e6f24fa9dc4d061a255be92987ebd3120c9d9c86
Patypatii <patrickwambugu707@gmail.com>
2025-07-07 08:37:24 +0300

    complted testimonials moobile view

--- frontend/src/sections/Testimonials.tsx
+++ frontend/src/sections/Testimonials.tsx
@@ -43,23 +43,26 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return <div className="py-16">
+
     <div className="container">
       <SectionHeader eyebrow="Happy clients" title="What Clients Say About Me" description="Don't just take my words for it. See what my clients have to say about my work." />
-      <div>
-        {testimonials.map(testimonial => (
-          <Card key={testimonial.name}>
-            <div className="flex">
-              <div >
-              <Image src={testimonial.avatar} alt={testimonial.name} />
+      <div className="mt-16  flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="flex gap-8 flex-none">
+          {testimonials.map(testimonial => (
+            <Card key={testimonial.name} className="max-w-xs">
+              <div className="flex gap-4 items-center">
+                <div className="size-14 bg-gray-700 inline-flex justify-center rounded-full flex-shrink-0" >
+                  <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
+                </div>
+                <div>
+                  <div className="font-semibold">{testimonial.name}</div>
+                  <div className="text-sm text-white/40">{testimonial.position}</div>
+                </div>
               </div>
-              <div>
-                <div>{testimonial.name}</div>
-                <div>{testimonial.position}</div>
-              </div>
-            </div>
-            <p>{testimonial.text}</p>
-          </Card>
-        ))}
+              <p className="mt-4 text-sm">{testimonial.text}</p>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   </div>;
